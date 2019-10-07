ilia-kats/RiboSeqTools

commit d464470cc7ed843fd5e24d4bd4569c1be23ba704

experimental feature to allow a user-defined ref column to be used for gene names
this is useful in cases where a gene has both a systematic and a common name
and the common name should be exposed to users in plots and function calls,
while the systematic name still serves as unique identifier

R/binding_scores.R
@@ -86,6 +86,7 @@ binding_scores <- function(data, sample1, sample2, bin, window_size, skip_5prime
                                      bin=bin,
                                      window_size=window_size,
                                      conf.level=conf.level,
+                                     ignore_genecol=TRUE,
                                      BPPARAM=bpparam) %>%
         dplyr::bind_rows(.id='gene') %>%
         {suppressWarnings(dplyr::inner_join(., get_reference(data), by='gene'))} %>%
@@ -127,7 +128,8 @@ binding_scores <- function(data, sample1, sample2, bin, window_size, skip_5prime
         dplyr::group_by(exp, rep) %>%
         dplyr::mutate(rank=dplyr::dense_rank(desc(lo_CI))) %>%
         dplyr::ungroup() %>%
-        dplyr::mutate(gene=as.factor(gene))
+        map_df_genenames(data) %>%
+        dplyr::mutate(scores, exp=as.factor(exp), rep=as.factor(rep))
 }
 
 #' @importFrom rmutil dbetabinom
@@ -362,9 +364,10 @@ test_binding <- function(data, window_size, bpparam=BiocParallel::bpparam()) {
         }, .id='rep')
     }, rawdata, bgdata, names(rawdata), SIMPLIFY=FALSE, BPPARAM=bpparam) %>%
         dplyr::bind_rows(.id='exp') %>%
+        mutate(exp=as.factor(exp), rep=as.factor(rep)) %>%
         group_by(exp, rep) %>%
         mutate(p.adj=p.adjust(pval, method='BY')) %>%
-        ungroup()
+        ungroup() %>%
     set_binding_pvalues(data, pvals)
 }
 
@@ -416,5 +419,6 @@ get_binding_positions <- function(data, fdr=0.01) {
             tibble::tibble(start=start, end=end, width=end - start + 1) %>%
                 mutate(!!bgmodel$sample1 := purrr::map2_int(start, end, function(s,e)sum(s1[s:e])), !!bgmodel$sample2 := purrr::map2_int(start, end, function(s,e)sum(s2[s:e])))
         }) %>%
+        map_df_genenames(data) %>%
         dplyr::ungroup()
 }

R/defaults.R
@@ -10,7 +10,8 @@ get_default_param <- function(serp_data, param, error=TRUE) {
     param
 }
 
-.defaults <- list(bin='byaa',
+.defaults <- list(genename_column="gene",
+                  bin='byaa',
                   window_size=45,
                   plot_ylim_enrichment=c(2^(-3), NA),
                   plot_ybreaks_enrichment=scales::log_breaks(base=2),
@@ -23,6 +24,7 @@ get_default_param <- function(serp_data, param, error=TRUE) {
 #' Named list of default values used in various functions. Usually, the default can be overriden by an argument
 #' to the respective function. Currently, the following defaults are recognized:
 #' \describe{
+#'      \item{genename_column}{Which column of the reference table to use for gene names.}
 #'      \item{bin}{Bin level, one of \code{byaa} for codon-level binning or \code{bynuc} for nucleotide-level
 #'          binning (corresponds to no binning).}
 #'      \item{sample1}{Sample to use in the numerator of enrichment calculations.}

R/import.R
@@ -125,7 +125,10 @@ load_experiment <- function(..., .ref, .bin=c('bynuc', 'byaa'), .exclude=NULL) {
 #'          \item{length}{ORF length in nucleotides.}
 #'      }
 #'      If all input files are HDF5 files, this argument can be missing, in which case a refence is
-#'      created from the union of all inputs files.
+#'      created from the union of all input files. For this to work, each HDF5 data set must have
+#'      an attribute \code{cds_length}. Other HDF5 attributes will be included as additional columns. If
+#'      HDF5 datasets have an attribute \code{gene}, the corresponding column will be named \code{gene_alt}
+#'      to avoid conflicts with the \code{gene} column created from dataset names.
 #' @param normalize Normalize the read counts to library size? Output will then be in RPM.
 #' @param bin Bin the data. \code{bynuc}: No binning (i.e. counts per nucleotide). \code{byaa}: Bin by residue.
 #' @param exclude Genes to exclude in all future analyses. This genes will also be excluded from total read count

R/metagene.R
@@ -142,7 +142,6 @@ make_aligned_mats <- function(data, align, lengths, pwidth, filter=NULL, binwidt
     }
     BiocParallel::bplapply(data, function(d) {
         .filter <- filter
-        stopifnot(is.null(.filter) || length(.filter) == length(align))
         if (is.null(.filter))
             .filter <- rownames(d)
         .filter <- intersect(.filter, names(align))
@@ -246,7 +245,7 @@ metagene_profile <- function(d, profilefun, len, bin, refs, extrapars=list(), ex
                 }, rownames(m), refs[rownames(m)]))
             }
         }, mats, len, SIMPLIFY=FALSE)
-    } else if (all(is.numeric(align))) {
+    } else if (is.numeric(align)) {
         mats <- make_aligned_mats(mats, align, refs, len, binwidth=1, binmethod='sum')
     } else {
         rlang::abort("unrecognized align parameter")
@@ -341,18 +340,24 @@ metagene_profiles <- function(data, ...) {
 metagene_profiles.serp_data <- function(data, profilefun, len, bin, filter=NULL, binwidth=1, binmethod=c('sum', 'mean'), normalizefun=NULL, align=c('start', 'stop'), nboot=100, bpparam=BiocParallel::bpparam()) {
     bin <- get_default_param(data, bin)
     binmethod <- match.arg(binmethod)
-    refs <- setNames(get_reference(data)$length, get_reference(data)$gene)
+    ref <- get_reference(data)
+    refs <- setNames(ref$length, ref$gene)
     exclude <- excluded(data)
     if (bin == 'byaa')
         refs <- refs / 3
+    if (is.numeric(align))
+        names(align) <- map_genenames(data, names(align))
     d <- purrr::imap_dfr(get_data(data), function(d, exp) {
         purrr::imap_dfr(d, function(d, rep) {
             .filter <- filter
             if (is.list(.filter)) {
                 .filter <- .filter[[exp]]
                 if (is.list(.filter))
-                .filter <- .filter[[rep]]
+                    .filter <- .filter[[rep]]
+
             }
+            .filter <- map_genenames(data, .filter)
+
             metagene_profile(d,
                              profilefun,
                              len,

R/treemaps.R
@@ -46,7 +46,8 @@ plot_treemap <- function(data, exp, rep, sample, geneclass, title='', palette="S
         dplyr::left_join(geneclass, by='gene') %>%
         tidyr::replace_na(list(class='unknown')) %>%
         dplyr::mutate(class=factor(class, classes, ordered=TRUE)) %>%
-        dplyr::filter(!(gene %in% excluded(data)[[exp]]))
+        dplyr::filter(!(gene %in% excluded(data)[[exp]])) %>%
+        map_df_genenames(data, replace_genecol=TRUE)
 
     maxreadsums <- dplyr::group_by(reads_per_gene_sample, class) %>%
         dplyr::summarize(s=sum(1/(read_sum + 1))) %$%

R/utils_data.R
@@ -32,23 +32,29 @@ normalize <- function(data) {
 #' Calculate total read counts per gene
 #'
 #' @param data A \code{serp_data} object.
-#' @return A \link[tibble]{tibble} with columns \code{exp}, \code{rep}, \code{sample}, \code{counts}, and \code{RPM}
+#' @return A \link[tibble]{tibble} with columns \code{exp}, \code{rep}, \code{sample}, \code{counts},
+#'      and \code{RPM}. If \code{\link[=defaults]{genename_column}} is not \code{"gene"}, the tibble will
+#'      in additon contain \code{\link[=defaults]{genename_column}}.
 #' @export
 get_genecounts <- function(data) {
     check_serp_class(data)
     if (is_normalized(data))
         rlang::abort("normalized data given")
 
-    purrr::map2_dfr(get_data(data), get_total_counts(data)[names(get_data(data))], function(exp, texp) {
+    exclude <- excluded(data)
+    purrr::pmap_dfr(list(exp=get_data(data), texp=get_total_counts(data)[names(get_data(data))], nexp=names(get_data(data))), function(exp, texp, nexp) {
         purrr::map2_dfr(exp, texp[names(exp)], function(rep, trep) {
             purrr::map2_dfr(rep, trep[names(rep)], function(sample, tsample) {
                 touse <- names(sample)[1]
                 Matrix::rowSums(sample[[touse]]) %>%
                     tibble::enframe(name="gene", value="counts") %>%
+                    dplyr::filter(!(gene %in% exclude[nexp])) %>%
                     dplyr::mutate(RPM=counts / tsample * 1e6)
             }, .id="sample")
         }, .id="rep")
-    }, .id="exp")
+    }, .id="exp") %>%
+        map_df_genenames(data) %>%
+        dplyr::mutate(exp=as.factor(exp), rep=as.factor(rep), sample=as.factor(sample))
 }
 
 #' Threshold data using the elbow method
@@ -285,7 +291,7 @@ get_background_model <- function(data) {
 #' @export
 get_binding_pvalues <- function(data) {
     check_serp_class(data)
-    data$binding_pvals
+    map_df_genenames(data$binding_pvals, data)
 }
 
 set_reference <- function(data, ref) {
@@ -329,7 +335,7 @@ set_downsampled <- function(data, downsampled) {
 
 set_excluded <- function(data, exclude) {
     check_serp_class(data)
-    data$excluded <- exclude
+    data$excluded <- purrr::map(exclude, partial(map_genenames, data=data, ...=), exclude)
     data
 }
 
@@ -358,6 +364,47 @@ calc_total_counts <- function(data, what=NULL) {
             })
         })
     })
+}
+
+get_gene_ref_idx <- function(data, gene, ignore_genecol=FALSE, error=TRUE) {
+    check_serp_class(data)
+    genecol <- ifelse(ignore_genecol, "gene", get_default_param(data, genename_column))
+    idx <- which(get_reference(data)[[genecol]] == gene)
+    if (!length(idx) && !ignore_genecol)
+        idx <- which(get_reference(data)$gene == gene)
+    if (!length(idx)) {
+        if (error)
+            rlang::abort("unknown gene")
+        else
+            idx <- NA_integer_
+    } else if (length(idx) > 1) {
+        rlang::abort(sprintf("Ambiguous gene name given. The default name column '%s' probably contains duplicate entries.", genecol))
+    }
+    idx
+}
+
+map_genenames <- function(data, genes) {
+    check_serp_class(data)
+    ref <- get_reference(data)
+    if (!all(genes %in% ref$gene)) {
+        genecol <- get_default_param(data, genename_column)
+        idx <- match(genes, ref[[genecol]])
+        genes[!is.na(idx)] <- ref$gene[idx[!is.na(idx)]]
+    }
+    genes
+}
+
+map_df_genenames <- function(df, data, replace_genecol=FALSE, replace_na=TRUE) {
+    if ((genecol <- get_default_param(data, genename_column)) != "gene") {
+        ref <- dplyr::select(get_reference(data), gene, !!genecol)
+        if (replace_na)
+            ref <- dplyr::mutate(ref, !!genecol:=dplyr::if_else(is.na(!!rlang::sym(genecol)), gene, !!rlang::sym(genecol)))
+        df <- dplyr::right_join(ref, df)
+        if (replace_genecol)
+            df <- dplyr::mutate(df, gene=!!genecol) %>%
+                dplyr::select(-!!genecol)
+    }
+    df
 }
 
 #' @export
@@ -496,7 +543,7 @@ combine_breaks <- function(x, y) {
 combine_defaults <- function(x, y) {
     out <- list()
     if (isTRUE(x$bin != y$bin))
-        out$bin <- 'byaa'
+        out$bin <- .defaults$bin
     else
         out$bin <- x$bin
 
@@ -507,6 +554,11 @@ combine_defaults <- function(x, y) {
 
     if (isTRUE(x$window_size == y$window_size))
         out$window_size <- x$window_size
+
+    if (isTRUE(x$genename_column != y$genename_column))
+        out$genename_column <- .defaults$genename_column
+    else
+        out$genename_column <- x$genename_column
 
     out$plot_ylim_rpm <- combine_limits(x$plot_ylim_rpm, y$plot_ylim_rpm)
     out$plot_ybreaks_rpm <- combine_breaks(x$plot_ybreaks_rpm, y$plot_ybreaks_rpm)

R/utils_enrichment.R
@@ -63,6 +63,9 @@ pois_ci <- function(sample, conf.level=0.95) {
 #' @param window_size Neighborhood size in nucleotides. Will be automatically converted to codons if binning
 #'      by codons.
 #' @param conf.level Confidence level.
+#' @param ignore_genecol If \code{TRUE}, assumes that the given \code{gene} is the gene identifier contained
+#'      in the \code{gene} column of the reference table. If \code{FALSE}, assumes that the given \code{gene}
+#'      corresponds to the \code{genename_column} setting in the \link{defaults}.
 #' @return A \link[tibble]{tibble} with the following columns: \describe{
 #'      \item{exp}{Experiment name.}
 #'      \item{rep}{Replicate name.}
@@ -80,7 +83,7 @@ pois_ci <- function(sample, conf.level=0.95) {
 #'      \item{hi_CI}{Upper confidence bound.}
 #'}
 #' @export
-binom_ci_profile <- function(data, gene, sample1, sample2, exp, rep, bin, window_size, conf.level=0.95) {
+binom_ci_profile <- function(data, gene, sample1, sample2, exp, rep, bin, window_size, conf.level=0.95, ignore_genecol=FALSE) {
     check_serp_class(data)
     stopifnot(!is_normalized(data))
 
@@ -96,8 +99,8 @@ binom_ci_profile <- function(data, gene, sample1, sample2, exp, rep, bin, window
     }
     binmissing <- missing(bin)
 
-    idx <- which(get_reference(data)$gene == gene)
-    if (!length(idx)) rlang::abort("unknown gene")
+    idx <- get_gene_ref_idx(data, gene, ignore_genecol)
+    gene <- get_reference(data)$gene[idx]
 
     genelen <- get_reference(data)$length[idx]
     cdslen <- get_reference(data)$cds_length[idx]
@@ -155,6 +158,9 @@ binom_ci_profile <- function(data, gene, sample1, sample2, exp, rep, bin, window
 #' @param window_size Neighborhood size in nucleotides. Will be automatically converted to codons if binning
 #'      by codons.
 #' @param conf.level Confidence level.
+#' @param ignore_genecol If \code{TRUE}, assumes that the given \code{gene} is the gene identifier contained
+#'      in the \code{gene} column of the reference table. If \code{FALSE}, assumes that the given \code{gene}
+#'      corresponds to the \code{genename_column} setting in the \link{defaults}.
 #' @return A \link[tibble]{tibble} with the following columns: \describe{
 #'      \item{exp}{Experiment name.}
 #'      \item{rep}{Replicate name.}
@@ -172,7 +178,7 @@ binom_ci_profile <- function(data, gene, sample1, sample2, exp, rep, bin, window
 #'      \item{hi_CI}{Upper confidence bound for RPM.}
 #'}
 #' @export
-pois_ci_profile <- function(data, gene, samples, exp, rep, bin, window_size, conf.level=0.95) {
+pois_ci_profile <- function(data, gene, samples, exp, rep, bin, window_size, conf.level=0.95, ignore_genecol=FALSE) {
     check_serp_class(data)
     if (is_normalized(data))
         rlang::abort("Need count data")
@@ -183,8 +189,8 @@ pois_ci_profile <- function(data, gene, samples, exp, rep, bin, window_size, con
     binmissing <- missing(bin)
     samplesmissing <- missing(samples)
 
-    idx <- which(get_reference(data)$gene == gene)
-    if (!length(idx)) rlang::abort("unknown gene")
+    idx <- get_gene_ref_idx(data, gene, ignore_genecol)
+    gene <- get_reference(data)$gene[idx]
 
     genelen <- get_reference(data)$length[idx]
     cdslen <- get_reference(data)$cds_length[idx]

man/binom_ci_profile.Rd
@@ -5,7 +5,7 @@
 \title{Calculate position-wise enrichment confidence intervals}
 \usage{
 binom_ci_profile(data, gene, sample1, sample2, exp, rep, bin, window_size,
-  conf.level = 0.95)
+  conf.level = 0.95, ignore_genecol = FALSE)
 }
 \arguments{
 \item{data}{A \code{serp_data} object.}
@@ -26,6 +26,10 @@ binom_ci_profile(data, gene, sample1, sample2, exp, rep, bin, window_size,
 by codons.}
 
 \item{conf.level}{Confidence level.}
+
+\item{ignore_genecol}{If \code{TRUE}, assumes that the given \code{gene} is the gene identifier contained
+in the \code{gene} column of the reference table. If \code{FALSE}, assumes that the given \code{gene}
+corresponds to the \code{genename_column} setting in the \link{defaults}.}
 }
 \value{
 A \link[tibble]{tibble} with the following columns: \describe{

commit ba7a96e817da7643f24c46881c384add581896ed

fix binding p-value calculation (bug introduced in d464470c)

R/binding_scores.R
@@ -367,7 +367,7 @@ test_binding <- function(data, window_size, bpparam=BiocParallel::bpparam()) {
         mutate(exp=as.factor(exp), rep=as.factor(rep)) %>%
         group_by(exp, rep) %>%
         mutate(p.adj=p.adjust(pval, method='BY')) %>%
-        ungroup() %>%
+        ungroup()
     set_binding_pvalues(data, pvals)
 }
 